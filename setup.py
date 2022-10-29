cbf8d27851c78dced2ce35c7b5798c69acbc1f42
Gianluca Sposito <70981929+gianlucasposito@users.noreply.github.com>
2022-10-29 09:15:51 +0200

    Add files via upload

--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 from setuptools import setup, find_packages
 
-VERSION = '1.01' 
-DESCRIPTION = 'Neural network out-of-control signal interpretation in multiple stream processes'
+VERSION = '2.28' 
+DESCRIPTION = 'Neural network based control charting for multiple stream processes'
 
 def readme():
     with open('README.md',  encoding="cp437", errors='ignore') as f:
@@ -10,18 +10,18 @@ def readme():
 # Setting up
 
 setup(
-        name="NN4OCMSP", 
+        name="NN4MSP", 
         version=VERSION,
         author='Antonio Lepore, Biagio Palumbo, Gianluca Sposito',
         author_email='antonio.lepore@unina.it, biagio.palumbo@unina.it, gianluca.sposito@unina.it',
         description=DESCRIPTION,
         long_description=readme(),
         long_description_content_type="text/markdown",
-        url='https://github.com/unina-sfere/NN4OCMSP',
+        url='https://github.com/unina-sfere/NN4MSP',
         license='GNU General Public License v3.0',
         packages=find_packages(),
         install_requires=['numpy', 'matplotlib', 'sklearn', 'tensorflow', 'keras'],
-        keywords=['Out-of-control signal interpretation', 'Artificial neural networks', 'Multiple stream process', 'Multi-label classification', 'Railway HVAC systems'],
+        keywords=['Multiple stream process', 'Artificial neural networks', 'Statistical process control', 'Multilayer perceptron', 'Railway HVAC systems'],
         classifiers=[
             "Intended Audience :: Education",
             "Intended Audience :: Science/Research",
